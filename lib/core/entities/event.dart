cb349acdd21298ecca8d9d30b985a342a1b36632
kmzr34 <unexpired8493@gmail.com>
2023-10-16 20:23:51 +0300

    sample data

--- lib/core/entities/event.dart
+++ lib/core/entities/event.dart
@@ -4,16 +4,18 @@ class Event {
   final String? id;
   final String? name;
   final DateTime? date;
+  final String? location;
   final String? venue;
   final List<Ticket>? tickets;
+  final String? description;
 
   Event({
     required this.id,
     required this.name,
     required this.date,
+    required this.location,
     required this.venue,
     required this.tickets,
+    required this.description,
   });
 }
-
-
---
+++ lib/presentation/widgets/bottom_nav.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  BottomNav({required this.selectedIndex, required this.onItemTapped});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _BottomNavState createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: widget.selectedIndex,
+      onTap: widget.onItemTapped,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
