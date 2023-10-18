4c63d7ef84cef88bc436583b3a29cc1e56846d04
kmzr34 <unexpired8493@gmail.com>
2023-10-18 12:49:53 +0300

    firebase initialization worked!!!

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:venti/presentation/pages/home.dart';
 import 'package:venti/presentation/pages/login.dart';
 import 'package:venti/presentation/pages/profile.dart';
 import 'package:venti/presentation/pages/register.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
